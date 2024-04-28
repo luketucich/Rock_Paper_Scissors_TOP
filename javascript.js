7f0a7e57dcaafbf09b3b0379a02a0a38ffd995cd
luketucich <ltucich@gmail.com>
2024-04-27 22:35:19 -0400

    I finished the game. I forgot to commit more frequently because I was so locked in.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+// Randomly assigns a choice to the computer
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3);
     if (computerChoice == 0) {
@@ -9,49 +10,97 @@ function getComputerChoice() {
     }
 }
 
+// Prompts the player with their choice
 function getHumanChoice() {
     let humanChoice = prompt("Rock, Paper, or Scissors?");
     return humanChoice.toLowerCase();
 }
 
-let humanScore = 0;
-let computerScore = 0;
-const humanSelection = getHumanChoice;
-const computerSelection = getComputerChoice;
+// Assigns a variable to the human and computer's choice.
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
-function playRound(humanChoice, computerChoice) {
-    // H-Rock vs C-Paper
-    if (humanChoice == "rock" && computerChoice == "paper") {
-        console.log("You lose! Paper beats rock!");
-        computerScore = computerScore + 1;
+function playGame() {
+    // This function determines who wins a single round and updates the score.
+    function playRound(humanChoice, computerChoice) {
+        // H-Rock vs C-Paper
+        if (humanChoice == "rock" && computerChoice == "paper") {
+            console.log("You lose! Paper beats rock!");
+            computerScore = computerScore + 1;
 
-        // H-Rock vs C-Scissors
-    } else if (humanChoice == "rock" && computerChoice == "scissors") {
-        console.log("You win! Rock beats scissors!");
-        humanScore = humanScore + 1;
+            // H-Rock vs C-Scissors
+        } else if (humanChoice == "rock" && computerChoice == "scissors") {
+            console.log("You win! Rock beats scissors!");
+            humanScore = humanScore + 1;
 
-        // H = C (tie)
-    } else if (humanChoice == computerChoice) {
-        console.log(`You both picked ${humanChoice}! Nobody wins.`)
+            // H = C (tie)
+        } else if (humanChoice == computerChoice) {
+            console.log(`You both picked ${humanChoice}! Nobody wins.`)
 
-        // H-Scissors vs C-Rock
-    } else if (humanChoice == "scissors" && computerChoice == "rock") {
-        console.log("You lose! Rock beats scissors!");
-        computerScore = computerScore + 1;
+            // H-Scissors vs C-Rock
+        } else if (humanChoice == "scissors" && computerChoice == "rock") {
+            console.log("You lose! Rock beats scissors!");
+            computerScore = computerScore + 1;
 
-        // H-Scissors vs C-Paper
-    } else if (humanChoice == "scissors" && computerChoice == "paper") {
-        console.log("You win! Scissors beats paper!");
-        humanScore = humanScore + 1;
+            // H-Scissors vs C-Paper
+        } else if (humanChoice == "scissors" && computerChoice == "paper") {
+            console.log("You win! Scissors beats paper!");
+            humanScore = humanScore + 1;
 
-        // H-Paper vs C-Rock
-    } else if (humanChoice == "paper" && computerChoice == "rock") {
-        console.log("You win! Paper beats rock!");
-        humanScore = humanScore + 1;
+            // H-Paper vs C-Rock
+        } else if (humanChoice == "paper" && computerChoice == "rock") {
+            console.log("You win! Paper beats rock!");
+            humanScore = humanScore + 1;
 
-        // H-Paper vs C-Scissors
-    } else if (humanChoice == "paper" && computerChoice == "scissors") {
-        console.log("You lose! Scissors beats paper!");
-        computerScore = computerScore + 1;
+            // H-Paper vs C-Scissors
+        } else if (humanChoice == "paper" && computerChoice == "scissors") {
+            console.log("You lose! Scissors beats paper!");
+            computerScore = computerScore + 1;
+        }
+        return
+    }
+
+    // As you may have guessed, this sets the initial scores to 0.
+    let humanScore = 0;
+    let computerScore = 0;
+
+    // This runs the first round and displays the score.
+    playRound(humanSelection, computerSelection);
+    console.log(`${humanScore} to ${computerScore}`)
+
+    // This runs the second round, updates new selections, and displays the score.
+    const humanSelection2 = getHumanChoice();
+    const computerSelection2 = getComputerChoice();
+    playRound(humanSelection2, computerSelection2);
+    console.log(`${humanScore} to ${computerScore}`)
+
+    // This runs the third round, updates new selections, and displays the score.
+    const humanSelection3 = getHumanChoice();
+    const computerSelection3 = getComputerChoice();
+    playRound(humanSelection3, computerSelection3);
+    console.log(`${humanScore} to ${computerScore}`)
+
+    // This runs the fourth round, updates new selections, and displays the score.
+    const humanSelection4 = getHumanChoice();
+    const computerSelection4 = getComputerChoice();
+    playRound(humanSelection4, computerSelection4);
+    console.log(`${humanScore} to ${computerScore}`)
+
+    // This runs the final round, updates new selections, and displays the score.
+    const humanSelection5 = getHumanChoice();
+    const computerSelection5 = getComputerChoice();
+    playRound(humanSelection5, computerSelection5);
+    console.log(`${humanScore} to ${computerScore}`)
+
+    // Announces the winner of the game.
+    if (humanScore > computerScore) {
+        console.log(`The final score was ${humanScore} to ${computerScore}.`)
+        console.log("You Win!");
+    } else {
+        console.log(`The final score was ${humanScore} to ${computerScore}.`)
+        console.log("Computer Wins!");
     }
 }
+
+// Runs the game!
+playGame();
